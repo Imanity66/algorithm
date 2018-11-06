d7f6f4b11045a5d5b732a226ada4b788065efb5b
Imanity66 <44795983+Imanity66@users.noreply.github.com>
2018-11-06 00:12:41 -0600

    Add files via upload

---
+++ binarySearchTree.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 21:21:48 2018
+
+@author: Imanity
+"""
+
+class _Node(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = self.right = None
+        
+class BinarySearchTree(object):
+    def __init__(self):
+        self.__root = None
+        
+    def __query(self, root ,key):
+        if not root:
+            return None
+        
+        if key < root.key:
+            return self.__query(root.left, key)## 只在return的时候用到递归，称为tail recursion  尾递归， 很容易写成while loop
+        elif key > root.key:
+            return self.__query(root.right, key)
+        else:
+            return root.value
+        
+    def query(self, key):
+        return self.__query(self.__root, key)
+    
+    #循环写法
+    def query(self, key):
+        curr = self.__root
+        while curr:
+            if key < curr.key:
+                curr = curr.left
+            elif key > curr.key:
+                curr = curr.right
+            else:
+                return curr.value
+        return None
+    
+    def FindMinimum(root):
+        if not root:
+            return None
+        return FindMinimum(root.left) or root
+    
+    def FindFirstLargerThanTarget(root, target):
+        if not root:
+            return None
+        if root.value == target: #往右找
+            return FindMinimum(root.right)
+        elif root.value < target:##往右找
+            return FindFirstLargerThanTarget(root.right, target)
+        else:
+            return FindFirstLargerThanTarget(root.left, target) or root
+        
+    def __insert(self, root, key, value):
+        ##return the root node of the modified tree after insertion
+        if not root:##为空直接插
+            return _Node(key, value)
+        if root.key == key:
+            root.value = value
+            
+        elif root.key < key:
+            root.right = self.__insert(root.right, key, value)
+        else:
+            root.left = self.__insert(root.left, key, value)
+            
+        return root
+    
+    
+    
+    def __deleteMin(self, root):
+        if not root.left:
+            return root.right
+        
+        root.left = self.__deleteMin(root.left)
+        return root
+        
+        
+        
+        
+    def __delete(self,root,key):
+        #return the root node of the modified tree after deletion
+        if not root:
+            return None
+        
+        if root.key < key:
+            root.right = self.__delete(root.right, key)
+            
+        elif root.key > key:
+            root.left = self.__delete(root.left, key)
+        else:
+            if not root.right:#只有左子树
+                return root.left
+            if not root.left:#只有右子树
+                return root.right
+            #
+            min_node = FindMinimum(root.right)
+            t = root
+            root = min_node
+            
+            root.right = self.__deleteMin(t.right)
+            root.left = t.left
+        
