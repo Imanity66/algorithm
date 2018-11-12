c85eb98f6e4c8aff4891105ee91bc624d1b0a1d0
Jiaqi Li <44795983+Imanity66@users.noreply.github.com>
2018-11-12 02:20:51 -0600

    Update string.py

--- string.py
+++ string.py
@@ -5,7 +5,7 @@ Created on Sun Nov 11 23:56:43 2018
 @author: Imanity
 """
 
-
+1.
 #char removal, 从字符串里去掉某几个字符
 #快慢指针 ， 慢指针本身是个index,直接就是我们要的东西的位置，快指针每次都往后跑一格
 #Time:O(n), space:O(n)
@@ -54,8 +54,8 @@ def remove_space(str):
         lst.pop()
     return ''.join(lst)
 
-
-#char de-duplication
+2.
+#char de-duplication. given a sorted array
 
 'aaaabbbbcccdddcc' => 'abcdc'
 #法一,指针
@@ -84,12 +84,92 @@ def remove_duplicate(str):
     return ''.join(lst)
 
 
+# leave only two letters in duplicated section
+aaabbbccc -> aabbcc
+#指针步长改成2就完事了
+def remove_duplicate(str):
+    if not str or len(str) < 2:
+        return str
+    lst = list(str)
+    slow, fast = 2, 2
+    while fast < len(lst):
+        if lst[fast] != lst[slow - 2]:
+            lst[slow] = lst[fast]
+            slow += 1
+        fast += 1
+    return '',join(lst[:slow])
+
+3.
+#Reverse
+'I love Yahoo' -> 'Yahoo love I'
+注意输入的时候把string先转成list, string  immutable
+
+def reverse_helper(lst, left, right):
+    while left < right:
+        lst[left], lst[right] = lst[right], lst[left]
+        left += 1
+        right -= 1
+    return lst
+
+def reverse_string(string):
+    lst = list(string)
+    reverse_helper(lst, 0, len(lst) - 1)
+    i = 0
+    left = i
+    right = i
+    while i < len(lst):
+        if i == len(lst) - 1 or lst[i+1] == " ":
+            right = i  #下一个是空格或者到底right就到某个单词的尾巴
+            reverse_helper(lst, left, right)
+            left = i + 2  #left跳过space到下一个单词的第一个字母
+        i += 1
+    return ''.join(lst)
+
+
+abcdef -> efabcd,reverse(str,k)
+1)ef + abcd = efabcd
+2)reverse(k, end), reverse(0, k), reverse(0, end)
+
+def reverse(lst, k):
+    return reverse_helper(lst[k:(len(lst))], 0, len(lst[k:(len(lst))]) - 1) + reverse_helper(lst[0: k], 0, len(lst[0: k])-1)
+
+a = list('abcdef')
+k = 4
+print(reverse(a,k))
 
 
 
+4.
+#substring
+Robin-Karp : avg case:O(m+n) , worst case:O(m*n)
 
-
-
+char --> int, ord('a') = 97
+int --> char, chr(97) = 'a'
+class RabinKarp:
+    def __init__(self):
+        self.base = 26   # base for multiplication
+        self.mod = 997  # prime number
+        
+    def strstr(self, strr ,sub):
+        if len(sub) > len(strr):
+            return -1
+        hay_hash, nhash = 0, 0  #hay_hash对应长  nhash 对应短
+        power = 1
+        for i in range(len(sub)):#计算sub的哈希值
+            power = power * self.base % self.mod if i != 0 else 1
+            hay_hash = (hay_hash * self.base + ord(strr[i])) % self.mod
+            nhash = (nhash * self.base + ord(needle[i])) % self.mod
+        
+        for i in range(len(sub), len(strr)):
+            if hay_hash = nhash and sub == strr[i - len(sub):i]:
+                return i - len(sub)
+            hay_hash -= (power * ord(strr[i - len(sub)])) % self.mod
+            if hay_hash < 0:
+                hay_hash += self.mod
+            hay_hash = (hay_hash * self.base + ord(strr[i])) % self.mod
+        if hay_hash == nhash and sub == strr[len(strr) - len(sub):]:
+            return len(strr) - len(sub)
+        return -1
 
 
 
